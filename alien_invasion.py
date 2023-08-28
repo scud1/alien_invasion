a7f6b28c1fd4ef654113daa626d13e1e934ef2bf
Scott <mars.sim@gmail.com>
2023-08-28 16:45:32 -0700

    Increasing game speed over course of game.

--- alien_invasion.py
+++ alien_invasion.py
@@ -70,6 +70,9 @@ class AlienInvasion:
         """Start a new game when the player clicks Play."""
         button_clicked = self.play_button.rect.collidepoint(mouse_pos)
         if button_clicked and not self.game_active:
+            # Reset the game settings.
+            self.settings.initialize_dynamic_settings()
+        
             # Reset the game statistics.
             self.stats.reset_stats()
             self.game_active = True
@@ -131,6 +134,7 @@ class AlienInvasion:
             # Destroy existing bullets and create new fleet.
             self.bullets.empty()
             self._create_fleet()
+            self.settings.increase_speed()
                 
     def _update_aliens(self):
         """Check if the fleet is at an edge, then update positions."""
