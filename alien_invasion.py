daf83fd022a2346b3ca33343feac4919b620bde3
Scott <mars.sim@gmail.com>
2023-09-01 15:39:16 -0700

    Adding high score, level, and ships remaining.

--- alien_invasion.py
+++ alien_invasion.py
@@ -79,6 +79,8 @@ class AlienInvasion:
             # Reset the game statistics.
             self.stats.reset_stats()
             self.sb.prep_score()
+            self.sb.prep_level()
+            self.sb.prep_ships()
             self.game_active = True
             
             # Get rid of any remaining bullets and aliens.
@@ -138,12 +140,17 @@ class AlienInvasion:
             for aliens in collisions.values():
                 self.stats.score += self.settings.alien_points * len(aliens)
             self.sb.prep_score()
+            self.sb.check_high_score()
                 
         if not self.aliens:
             # Destroy existing bullets and create new fleet.
             self.bullets.empty()
             self._create_fleet()
             self.settings.increase_speed()
+            
+            # Increase level.
+            self.stats.level += 1
+            self.sb.prep_level()
                 
     def _update_aliens(self):
         """Check if the fleet is at an edge, then update positions."""
@@ -168,8 +175,9 @@ class AlienInvasion:
     def _ship_hit(self):
         """Respond to the ship being hit by an alien."""
         if self.stats.ships_left > 0:
-            # Decrement ships_left.
+            # Decrement ships_left and update the scoreboard.
             self.stats.ships_left -= 1
+            self.sb.prep_ships()
             
             # Get rid of any remaining bullets and aliens.
             self.bullets.empty()
